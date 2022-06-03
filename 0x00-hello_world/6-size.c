7220ed5e6d708c42485d5602ad89655b1cd4a6d9
Ngetich31801 <kipngetichjerry@gmail.com>
2022-06-03 05:30:59 -0700

    task5

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - my entry point
  * Description: prints all sizes in c
@@ -8,23 +7,13 @@
 int main(void)
 {
 	char a;
-
 	int b;
-
 	int long c;
-
 	int long long d;
-
 	printf("Size of a char: %zu byte(s)\n", sizeof(a));
-
 	printf("Size of an int: %zu byte(s)\n", sizeof(b));
-
 	printf("Size of a long int: %zu byte(s)\n", sizeof(c));
-
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(d));
-
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
-
 	return (0);
-
 }
