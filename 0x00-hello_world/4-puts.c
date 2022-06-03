b931852bf62250d63f8df873c5ae3b3d408c6c2c
Ngetich31801 <kipngetichjerry@gmail.com>
2022-06-03 05:08:19 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
 /**
-    * main - my entry point
-    * Description: prints "Programming is like building a multilingual puzzle"
- 
-    * Return: 0
- 
-    */
+ * main - my entry point
+ * Description: prints "Programming is like building a multilingual puzzle"
+ * Return: 0
+*/
 
 int main(void)
 
