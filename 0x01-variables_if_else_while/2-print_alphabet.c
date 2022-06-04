cb71864f469566301fa16900f8fd54fda22c8ab3
Ngetich31801 <kipngetichjerry@gmail.com>
2022-06-04 06:07:21 -0700

    tas2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -15,12 +15,12 @@ int main(void)
 
 
 
-			for (ch = 'a'; ch <= 'z'; ch++)
+		for (ch = 'a'; ch <= 'z'; ch++)
 
-						putchar(ch);
+					putchar(ch);
 
 					putchar('\n');
 
-						return (0);
+		return (0);
 
 }
