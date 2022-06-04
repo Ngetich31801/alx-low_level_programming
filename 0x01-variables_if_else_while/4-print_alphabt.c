54d82d5d438e2b5a309cc00d2e45fcea800d7f4b
Ngetich31801 <kipngetichjerry@gmail.com>
2022-06-04 07:21:36 -0700

    task4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,15 +1,15 @@
-nclude <stdio.h>
+include <stdio.h>
 
 /**
  * main - main function
  * Description:printing alphabet in lowercase
  * Return: 0
  */
-
 int main(void)
 
 {
 	int ch;
+
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		if (ch != 'q' && ch != 'e')
