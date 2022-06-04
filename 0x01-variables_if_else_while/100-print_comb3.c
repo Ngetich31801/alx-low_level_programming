69e862dd001fdde2129de199725670132da088c0
Ngetich31801 <kipngetichjerry@gmail.com>
2022-06-04 12:30:11 -0700

    task1

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,41 +6,19 @@
  int main(void)
 {
 	int i;
+	int j;
 
-		int j;
-
-
-
-			for (i = 0; i < 10; i++)
-
-					{
-
-								for (j = 0; j < 10; j++)
-
-											{
-
-															putchar(i + '0');
-
-																		putchar(j + '0');
-
-																					if (i == 9 && j == 9)
-
-																									{
-
-																														break;
-
-																																	}
-
-																								putchar(',');
-
-																											putchar(' ');
-
-																													}
-
-									}
-
-				putchar('\n');
-
-					return (0);
-
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+		putchar(j + '0');
+		if (i == 9 && j == 9)
+		{
+			break;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
 }
