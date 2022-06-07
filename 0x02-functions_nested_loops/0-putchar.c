9fe8796fa226493a265dafe552dda62c291865ce
Ngetich31801 <kipngetichjerry@gmail.com>
2022-06-07 08:06:04 -0700

    task1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main ()
+{
+	char ps = '1';
+       
+	while (ps)
+	{
+		putchar(ps);
+		ps++;
+	}
+	        putchar('\n');
+
+	return (0);
+}
+
+
+
