d3a421e56f884d347c2cfdc5e29304c83d959b16
Ngetich31801 <kipngetichjerry@gmail.com>
2022-06-08 15:01:44 -0700

    m

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,10 +2,11 @@
 
 /**
  * isalpha - checks for alphabetic characters
- * @c: charecter to check
+ * @c: character to check
  *
  * Return: 0 or 1
 */
+
 int isalpha(int c)
 {
 	if ((c >= "a" && c <= 'z') || (c >= 'A' && c <= 'Z'))
