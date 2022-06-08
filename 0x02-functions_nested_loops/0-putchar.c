2e659a6b82b11e7006e9a2bb27beb4e52b3f095c
Ngetich31801 <kipngetichjerry@gmail.com>
2022-06-08 03:01:52 -0700

    taso

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,19 @@
 #include "main.h"
 /**
- * main - prints _putchar
- *
- * Description: prints _putchar
- *
- * Return: 0 (Success)
+ * main- my entry
+ * description: program that prints _putchar
+ * Return : 0
  */
-
 int main(void)
 {
-	char text[9] = "_putchar";
-	 int i = 0;
-
-	 for (i = 0; i < 8; i++)
-	 {
-		  _putchar(text[i]);
-	 }
-	 _putchar('\n');
-
-	 return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
