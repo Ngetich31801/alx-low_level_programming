866c6908363387e972ce5e7d94b73f0560b62809
Ngetich31801 <kipngetichjerry@gmail.com>
2022-06-15 11:00:54 -0700

    task6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalizes all words of a string.
+ * @str: The string to be capitalized.
+ * Return: A pointer to the changed string.
+ */
+char *cap_string(char *str)
+{
+	int a = 0, i;
+	int cspc = 13;
+
+	char spc[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
+
+	while (s[a])
+	{
+		i = 0;
+
+		while (i < cspc)
+		{
+			if ((a == 0 || s[a - 1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
+				s[a] -= 32;
+
+			i++;
+		}
+		a++;
+	}
+	return (s);
+}
