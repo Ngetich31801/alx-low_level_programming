1333de4e8d8f7f7a7c87edaca7e8c287ad07402a
Ngetich31801 <kipngetichjerry@gmail.com>
2022-06-24 09:25:27 -0700

    task 1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,11 +8,11 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
+	int x;
 
-	for (i = 0; i < argc ; i++)
+	for (x = 0; x < argc ; x++)
 	{
-		printf("%s\n", argv[i]);
+		printf("%s\n", argv[x]);
 	}
 	return (0);
 }
