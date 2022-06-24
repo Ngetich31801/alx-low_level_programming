dec6723b98d10b4e296f3a82212f8fd67b0eb99a
Ngetich31801 <kipngetichjerry@gmail.com>
2022-06-24 10:13:09 -0700

    task2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+/**
+ * main -  print name
+ * @argc: int.
+ * @argv: char.
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int x;
+
+	for (x = 0 ; x < argc ; x++)
+	{
+		printf("%s\n", argv[x]);
+	}
+	return (0);
+}
+
+
