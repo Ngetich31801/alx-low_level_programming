d03ae01795ad7313785bbc6d77c7d54f847e78de
Ngetich31801 <kipngetichjerry@gmail.com>
2022-06-27 20:31:01 -0700

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * *_strdup - function to return pointer to string
+ * @str: pointer to string array input
+ * Return: pointer to string created
+ */
+char *_strdup(char *str)
+{
+	char *ptr;
+	int x;
+	int size;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	size = _strlen(str) + 1;
+	ptr = malloc(size * sizeof(char));
+
+
+	x = 0;
+	while (x < size)
+	{
+		if (ptr == NULL)
+		{
+			return (NULL);
+		}
+		ptr[x] = str[x];
+		x++;
+	}
+	return (ptr);
+}
