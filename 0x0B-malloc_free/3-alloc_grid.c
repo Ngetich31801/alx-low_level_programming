dea7a8e6dc8a35982f17e557d7e83623fa5538df
Ngetich31801 <kipngetichjerry@gmail.com>
2022-06-28 06:16:47 -0700

    ttaf

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * **alloc_grid - function to allocate memory to grid
+ * @width: int type
+ * @height: int type
+ * Return: grid of 0sor null
+ */
+int **alloc_grid(int width, int height)
+{
+	int a, b;
+	int **p;
+
+	if (width <= 0 || height <= 0)
+	{
+		return  (NULL);
+	}
+	p = malloc(height * sizeof(int *));
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (a = 0; a < height; a++)
+	{
+		p[a] = malloc(width * sizeof(int));
+		if (p[a] == NULL)
+		{
+			for (b = 0; b < a;  b++)
+				free(ptr[b]);
+			free(p);
+			return (NULL);
+		}
+		for (b = 0; b < width; b++)
+		{
+			ptr[a][b] = 0;
+		}
+	}
+	return (p);
+}
