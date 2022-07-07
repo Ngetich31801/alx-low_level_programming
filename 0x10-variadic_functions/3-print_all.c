6696c528a996fa16172f921c5a571d2f164a9023
Ngetich31801 <kipngetichjerry@gmail.com>
2022-07-07 12:19:34 -0700

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,20 +1,91 @@
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
+#include <stdio.h>
+#include "variadic_functions.h"
 #include <stdarg.h>
 
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
+/**
+ * op_c - Print character .
+ * @form: name va_list
+ * Return: Nothing.
+ */
+void op_c(va_list form)
+{
+	printf("%c", va_arg(form, int));
+}
 
 /**
- * struct format - Data type of a format.
- * @op: Format.
- * @f: Function.
+ * op_i - Print Integer
+ * @form: name va_list
+ * Return: Nothing.
  */
-typedef struct format
+
+void op_i(va_list form)
 {
-	char *op;
-	void (*f)(va_list all);
-}f;
-#endif
+	printf("%i", va_arg(form, int));
+}
+
+/**
+ * op_f - print FLoat numbers
+ * @form: name of va_list
+ * Return: Nothing.
+ */
+void op_f(va_list form)
+{
+	printf("%f", va_arg(form, double));
+}
+
+/**
+ * op_s -print string
+ * @form: name va_list
+ * Return: Nothing.
+ */
+void op_s(va_list form)
+{
+	char *str;
+
+	str = va_arg(form, char *);
+	if (str == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", str);
+}
+/**
+ * print_all - check the code for Holberton School students.
+ * @format: number of arguments in character format
+ * Return: Nothing.
+ */
+void print_all(const char * const format, ...)
+{
+	va_list all;
+	unsigned int i, j;
+	char *separator = "";
+
+	f ops[] = {
+		{"c", op_c},
+		{"i", op_i},
+		{"f", op_f},
+		{"s", op_s},
+	};
+
+	va_start(all, format);
+	i = 0;
+	while (format && format[i])
+	{
+		j = 0;
+		while (j < 4)
+		{
+			if (ops[j].op[0] == format[i])
+			{
+				printf("%s", separator);
+				separator = ", ";
+				ops[j].f(all);
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(all);
+}
