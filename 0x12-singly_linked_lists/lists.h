c00d23f18eea215bdcd6abeedec6688ec1e731a4
Ngetich31801 <99500497+Ngetich31801@users.noreply.github.com>
2022-07-15 23:01:11 +0300

    Update lists.h
    lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
@@ -11,7 +12,6 @@
  * @len: length of the string
  * @next: points to the next node
  * Description: singly linked list node structure
- * for Holberton project
  */
 
 typedef struct list_s
