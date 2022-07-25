beb291b9d699b15287e2a6550309e46664d7e817
Ngetich31801 <kipngetichjerry@gmail.com>
2022-07-25 13:51:57 -0700

    all

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,12 +7,12 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t x;
+	size_t i;
 
-	for (x = 0; h; x++)
+	for (i = 0; h; i++)
 	{
 		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 	}
-	return (x);
+	return (i);
 }
