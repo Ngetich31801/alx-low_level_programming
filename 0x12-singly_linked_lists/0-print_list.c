e29ab1ebc694ad982a979dd8ba70563a7a1ba7bf
Ngetich31801 <kipngetichjerry@gmail.com>
2022-07-25 13:21:42 -0700

    tasko

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: singly linked list.
+ * Return: number of elements in the list.
+ */
+size_t print_list(const list_t *h)
+{
+	size_t x;
+
+	for (x = 0; h; x++)
+	{
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+	}
+	return (x);
+}
