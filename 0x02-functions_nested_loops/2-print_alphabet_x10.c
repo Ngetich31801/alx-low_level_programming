e462d2307cbe952a7583faee8f8e7f9d72a77e13
Ngetich31801 <kipngetichjerry@gmail.com>
2022-07-28 05:11:32 -0700

    taskx

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - Ptrint alphabet x10 times
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char a;
+	int b = 0;
+
+	while (b <= 9)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+		b++;
+	}
+}
